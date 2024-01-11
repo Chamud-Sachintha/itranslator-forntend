8f9c0bce8185b26672e71b81c67281aba66fac9a
Chamud-Sachintha <sheraanmario777@gmail.com>
2024-01-11 11:19:19 +0530

    add dev server api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,8 @@
 export const environment = {
     // appURL: "http://localhost:8000/api/",
     // fileServerURL: "http://localhost:8001/images/",
+    // devServer: "http://localhost:8000/",
     fileServerURL: "https://clientapi.itranslate.lk/images/",
-    appURL: "https://adminapi.itranslate.lk/api/"
+    appURL: "https://adminapi.itranslate.lk/api/",
+    devServer: "https://adminapi.itranslate.lk/"
 };
